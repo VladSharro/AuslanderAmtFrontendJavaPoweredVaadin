b13f79e925bd2613e545a421ef1884a4d0108ba2
Vladislav Sharonin <sharon01@ads.uni-passau.de>
2023-12-28 22:32:34 +0000

    Adding date and remove some bugs

--- OCR/WorkContract.py
+++ OCR/WorkContract.py
@@ -9,12 +9,27 @@ from PIL import Image
 import numpy as np
 import re
 from difflib import SequenceMatcher
+from datetime import datetime
 
 
+
+# Path to the Tesseract executable
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
+
+
+def extract_date_after_word(text, word, lines_before=200):
+    pattern = re.compile(r'\d{1,2}/\d{1,2}/\d{4}')
+
+    lines = text.split('\n')
+    for i in range(max(0, len(lines) - lines_before), len(lines)):
+        match = pattern.search(lines[i])
+        if match:
+            return match.group(0)
+
+    return None
 
 
 def extract_number_after_word(text, word):
@@ -26,12 +41,13 @@ def extract_number_after_word(text, word):
         return None
 
 def extract_first_number_after_word(text, word):
-    pattern = re.compile(rf'{word}\D*(\d+)')
+    pattern = re.compile(rf'{word}\D*(\d+)', re.MULTILINE)
     match = pattern.search(text)
     if match:
         return int(match.group(1))
     else:
         return None
+
 
 def convert_pdf_to_images(pdf_path):
     doc = fitz.open(pdf_path)
@@ -41,8 +57,10 @@ def convert_pdf_to_images(pdf_path):
 
         image_list = page.get_images(full=True)
 
+        i = 0
+
         for img_index, img_info in enumerate(image_list):
-            xref = img_info[0]  # Get the index of the image
+            xref = img_info[0]
             base_image = doc.extract_image(xref)
 
             image_bytes = base_image["image"]
@@ -55,20 +73,20 @@ def convert_pdf_to_images(pdf_path):
             extracted_text = pytesseract.image_to_string(Image.fromarray(gray_image, 'L'))
 
 
-            target_word = "Arbeitszeit"
+            #target_word = "Arbeitszeit"
 
 
             #print(extracted_text)
 
-            # Поиск строки с заданным словом "Arbeitsvergitung" в качестве заголовка пункта
             if "Arbeitsvergitung" in extracted_text:
 
                 money = extract_number_after_word(extracted_text, "von")
+
+                #print(money)
                 #if number is not None:
                     #print(f"Found on Page {page_num + 1}, Image {img_index + 1}: Arbeitsvergitung {number}")
                     #print('=' * 30)
 
-                # Display the image
                 #cv2.imshow(f"Page {page_num + 1}, Image {img_index + 1}", image)
                 #cv2.waitKey(0)
                 #cv2.destroyAllWindows()
@@ -77,16 +95,28 @@ def convert_pdf_to_images(pdf_path):
             if "Arbeitszoit" in extracted_text:
 
 
-                hours = extract_first_number_after_word(extracted_text, target_word)
+                hours = extract_first_number_after_word(extracted_text, "Arbeitszoit")
+
+                #print(hours)
 
                 #if number is not None:
                     #print(f"Found on Page {page_num + 1}, Image {img_index + 1}: Arbeitszeit Number {number}")
                     #print('=' * 30)
 
-                # Display the image
                 #cv2.imshow(f"Page {page_num + 1}, Image {img_index + 1}", image)
                 #cv2.waitKey(0)
                 #cv2.destroyAllWindows()
+
+            if "Datum" in extracted_text:
+
+                date = extract_date_after_word(extracted_text, "Datum")
+
+
+                #print(date)
+
+            #i = i + 1
+
+
 
     final_count = money * hours * 4
 
@@ -94,16 +124,31 @@ def convert_pdf_to_images(pdf_path):
 
     doc.close()
 
-    return final_count
+    return final_count, date
+
+
 #pdf_path = "C:\\Users\\vlads\\test\\Arbeitsvertrag_Muster_Photo.pdf"
+
+
 
 image_data = os.environ.get("IMAGE_DATA")
 
 
+moneys, date = convert_pdf_to_images(pdf_path)
+
+today = datetime.now().date()
+
+target_date = datetime.strptime(date, "%d/%m/%Y").date()
+
+if target_date > today:
+    print("True")
+else:
+    print("False")
 
 
-moneys = convert_pdf_to_images(pdf_path)
 
-print(','.join([moneys]))
+
+
+print(','.join([moneys, date]))
 
 #print(moneys)
