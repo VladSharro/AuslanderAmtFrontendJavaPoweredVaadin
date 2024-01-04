b8df135585d8a50a862e2354a0ab59deae281f74
Kirill Borisov <k.borisov239@gmail.com>
2024-01-04 12:53:57 +0100

    hardcoded chrome version

--- Tests/chromedriver_download.py
+++ Tests/chromedriver_download.py
@@ -9,7 +9,8 @@ def chromedriver_download():
 
     r = get(latest_endpoint)
     version = r.text
-    url = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/" + version + "/linux64/chromedriver-linux64.zip"
+    #url = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/" + version + "/linux64/chromedriver-linux64.zip"
+    url = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/" + "120.0.6099.199" + "/linux64/chromedriver-linux64.zip"
 
     urlretrieve(url, filename)
     with ZipFile(filename, "r") as f:
