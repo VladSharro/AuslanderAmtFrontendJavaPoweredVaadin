6a1188aeeb618b21223f269046728ba969a8d9b7
Vladislav Sharonin <sharon01@ads.uni-passau.de>
2024-01-07 13:11:03 +0000

    Generate file

--- OCR/Final_File_Reader.py
+++ OCR/Final_File_Reader.py
@@ -1,1 +1,29 @@
+import PyPDF2
+from pdfminer.high_level import extract_text, extract_pages
+from pdfminer.layout import LTTextContainer, LTRect
+import re
 
+
+def extract_checkboxes_and_text(pdf_path):
+    pdf_file = open(pdf_path, 'rb')
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+
+    checkboxes = []
+    for page_num in range(pdf_reader.numPages):
+        page = pdf_reader.getPage(page_num)
+        annotations = page.get('/Annots')
+        if annotations:
+            for annot in annotations:
+                annot_obj = annot.getObject()
+                if annot_obj.get('/FT') == '/Btn' and annot_obj.get('/AS'):
+                    checkbox_status = annot_obj.get('/AS')
+                    checkboxes.append((page_num + 1, checkbox_status))
+
+
+    text = extract_text(pdf_path)
+    lines = text.split('\n')
+    numbered_lines = [(i + 1, line) for i, line in enumerate(lines)]
+
+
+        pdf_file.close()
+    return checkboxes, numbered_lines
