475a991864e58f55e4959a64c4ff8e9af1441f3f
Kirill Borisov <k.borisov239@gmail.com>
2024-01-20 22:28:12 +0100

    enrolment certificate units

--- OCR/tests/ocrTests.py
+++ OCR/tests/ocrTests.py
@@ -7,11 +7,14 @@
 
 import unittest
 from passport_tests import PassportOCRTest
+from enrolment_tests import EnrolmentOCRTests
 
 def suite():
     test_suite = unittest.TestSuite()
     passport_test = unittest.makeSuite(PassportOCRTest)
+    enrolment_test = unittest.makeSuite(EnrolmentOCRTests)
     test_suite.addTest(passport_test)
+    test_suite.addTest(enrolment_test)
 
     return test_suite
 
