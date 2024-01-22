ef77c903ba0e08168ad1794d21129a3f02432721
Kirill Borisov <k.borisov239@gmail.com>
2024-01-22 17:55:40 +0100

    api test automation

---
+++ Tests/api/api_tests.py
@@ -0,0 +1,50 @@
+import requests
+import json
+import unittest
+
+ENDPOINT_BASE_URL = "http://132.231.1.189:8080"
+
+class ApiEndpointTests(unittest.TestCase):
+    def test_passport_endpoint(self):
+        endpoint = "/get_data_from_passport"
+        h = {
+            "accept": "application/hal+json"
+        }
+        url = ENDPOINT_BASE_URL + endpoint
+
+        with open('./OCR/tests/data/pass.jpg', 'rb') as file:
+            f = { 'passport_image': ('pass.jpg', file, 'image/jpeg') }
+            r = requests.post(url, headers=h, files=f)
+
+        self.assertEqual (r.status_code, 200)
+    
+    def test_enrolment_endpoint(self):
+        endpoint = "/get_data_from_certificate_of_enrollment"
+        h = {
+            "accept": "application/hal+json"
+        }
+        url = ENDPOINT_BASE_URL + endpoint
+
+        with open('./OCR/tests/data/enrolment2.pdf', 'rb') as file:
+            f = { 'certificate_of_enrollment_image': ('enrolment2.pdf', file, 'application/pdf') }
+            r = requests.post(url, headers=h, files=f)
+        
+        self.assertEqual (r.status_code, 200)
+    
+    def test_health_endpoint(self):
+        endpoint = "/get_data_from_health_insurance_certificate"
+        h = {
+            "accept": "application/hal+json"
+        }
+        url = ENDPOINT_BASE_URL + endpoint
+
+        with open('./OCR/tests/data/health.pdf', 'rb') as file:
+            f = { 'health_insurance_certificate': ('health.pdf', file, 'application/pdf') }
+            r = requests.post(url, headers=h, files=f)
+        
+        response = json.loads(r.text)
+        print(response)
+        self.assertEqual (r.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
