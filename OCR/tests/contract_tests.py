67daa193d56190226717c3340adcadd2fe1fb24b
Kirill Borisov <k.borisov239@gmail.com>
2024-01-22 02:59:17 +0100

    contract units fixes

--- OCR/tests/contract_tests.py
+++ OCR/tests/contract_tests.py
@@ -6,35 +6,48 @@ import unittest
 import os
 import sys
 import base64
+import re
 
 path = os.path.abspath("./OCR")
 sys.path.append(path)
 import WorkContract
 
 class ContractOCRTest(unittest.TestCase):
+    extracted_data = []
+
     def setUp(self) -> None:
-        paths = ["./OCR/tests/data/contract2.pdf"]
+        # Add test data paths here
+        paths = ["./OCR/tests/data/contract.pdf", "./OCR/tests/data/contract2.pdf"]
         pdf_paths = []
         for p in paths:
             abspath = os.path.abspath(p)
             pdf_paths.append(abspath)
         
+        # Encode test PDFs
         self.encoded_pdfs = []
         for pdf_path in pdf_paths:
             with open(pdf_path, "rb") as pdf_file:
                 encoded_pdf = base64.b64encode(pdf_file.read())
                 self.encoded_pdfs.append(encoded_pdf)
-        
-        self.extracted_data = []
-        for pdf in self.encoded_pdfs:
-            pdf_data = WorkContract.convert_pdf_to_images(pdf)
-            self.extracted_data.append(pdf_data)
     
     def test_contract_processing(self):
         print("\nVerifying extracted dataset lengths...\n")
-        for dataset in self.extracted_data:
+        # Run a module under test over PDFs
+        for pdf in self.encoded_pdfs:
+            pdf_data = WorkContract.convert_pdf_to_images(pdf)
+            self.__class__.extracted_data.append(pdf_data)
+        
+        for dataset in self.__class__.extracted_data:
             print("Verifying length of the dataset: ", dataset)
             self.assertGreater(len(dataset), 0, "Extracted data list is empty")
+    
+    # Verify the format of contract end date
+    def test_enrolment_date_format(self):
+        print("\nVerifying format of date fields...\n")
+        pattern = re.compile('\d{2}.\d{2}.\d{4}')
+        for dataset in self.__class__.extracted_data:
+            end_date = dataset[1]
+            self.assertTrue(pattern.match(end_date), "Contract end date doesn't fit the format. Value under test " + end_date)
 
 if __name__ == '__main__':
     unittest.main()
