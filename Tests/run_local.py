45fa9d091cb8d32c30aa9be64a11a7ab1c01c2e2
Kirill Borisov <k.borisov239@gmail.com>
2024-01-23 01:50:42 +0100

    e2e image migration

--- Tests/run_local.py
+++ Tests/run_local.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from robot import run_cli
 
-args = ['--variable', 'BROWSER:Chrome', '--variable', 'DOMAIN:localhost:4200', '--argumentfile', './options.txt']
+# args = ['--variable', 'BROWSER:Chrome', '--variable', 'DOMAIN:localhost:4200', '--argumentfile', './options.txt']
+args = ['--variable', 'BROWSER:Chrome', '--variable', 'DOMAIN:132.231.1.189', '--argumentfile', 'options.txt']
 run_cli (args)
