f5d69a0eab1705b9731e7919d70d30c18b708607
Sharonin Vladislav <vladsharonin@gmail.com>
2024-01-27 17:32:43 +0100

    Some adjusment

--- src/main/java/com/example/appforauslenderamt/frotn/returnofthehope/StartView.java
+++ src/main/java/com/example/appforauslenderamt/frotn/returnofthehope/StartView.java
@@ -5,7 +5,7 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("startiew")
+@Route("startview")
 public class StartView extends VerticalLayout {
 
 
